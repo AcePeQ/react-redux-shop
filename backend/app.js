152202823fddbca630059e06e21f3a2e1cd6ed82
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-20 16:15:48 +0200

    feat(api): endpoint to get menu from the backend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+import fs from "node:fs/promises";
+
 import bodyParser from "body-parser";
 import express from "express";
 
@@ -6,6 +8,16 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+app.get("/meals", async (_, res) => {
+  const meals = await fs.readFile("./data/menu.json", "utf8");
+
+  if (!meals) {
+    res.status(404).json({ message: "Menu not found!" });
+  }
+
+  res.status(200).json(JSON.parse(meals));
+});
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
