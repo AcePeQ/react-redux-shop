78c0563b220a9680f29316ff6c9ceb0a9c6f2cab
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-20 17:19:27 +0200

    feat(ui | css): create simple routes and pages, add styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import Layout from "./components/Layout/Layout";
+import Homepage from "./pages/Homepage/Homepage";
+import MenuPage from "./pages/MenuPage/MenuPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+
 function App() {
-  return <div>Elo</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
