9b27c14c93df96370f50da1ed7a83b153180af45
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-21 12:31:06 +0200

    feat(ui): add VideoHero: attach background video, add 3 background videos

--- frontend/src/components/VideoHero/VideoHero.tsx
+++ frontend/src/components/VideoHero/VideoHero.tsx
@@ -8,6 +8,16 @@ function VideoHero() {
           Qusto <br /> Restaurant
         </h1>
       </div>
+      <video
+        className={styles.video}
+        width="100%"
+        height="100%"
+        autoPlay
+        muted
+        loop
+      >
+        <source src="/public/videos/videobg2.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
