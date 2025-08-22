b487b125ef53518fb5b6ce38ca6703492b6bd125
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-22 17:25:14 +0200

    feat(ui): create opening hours container

---
+++ frontend/src/components/OpeningHours/OpeningHours.tsx
@@ -0,0 +1,34 @@
+import styles from "./OpeningHours.module.css";
+
+const hoursArray = [
+  { day: "Mon", hours: "16:00 - 22:30" },
+  { day: "Tue", hours: "16:00 - 22:30" },
+  { day: "Wed", hours: "16:00 - 22:30" },
+  { day: "Thu", hours: "16:00 - 22:30" },
+  { day: "Fri", hours: "16:00 - 22:30" },
+  { day: "Sat & Sun", hours: "16:00 - 22:30" },
+];
+
+function OpeningHours() {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>
+          Opening <br /> Hours
+        </h2>
+
+        <div className={styles.hours}>
+          {hoursArray.map((item) => (
+            <p className={styles.hour_row}>
+              <span className={styles.day}>{item.day}</span>
+              <span aria- className={styles.dots}></span>
+              <span className={styles.hour}>{item.hours}</span>
+            </p>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default OpeningHours;
--- frontend/src/pages/ContactPage/ContactPage.tsx
+++ frontend/src/pages/ContactPage/ContactPage.tsx
@@ -1,5 +1,20 @@
+import OpeningHours from "../../components/OpeningHours/OpeningHours";
+import VideoHero from "../../components/VideoHero/VideoHero";
+import styles from "./ContactPage.module.css";
+
 function ContactPage() {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <VideoHero videoPath="/videos/videobg4.mp4" title="Contact" />
+
+      <div className={styles.inner_container}>
+        <OpeningHours />
+        <div>Box3</div>
+        <div>Box4</div>
+        <div>Box5</div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactPage;
