1e2905cc4cc32835a89c0fd122051399ef98858b
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-22 13:17:06 +0200

    feat(ui): complete filling back card informations

--- frontend/src/features/Menu/MenuCard/MenuCard.tsx
+++ frontend/src/features/Menu/MenuCard/MenuCard.tsx
@@ -11,6 +11,13 @@ function MenuCard() {
         <div className={styles.card_back}>
           <img className={styles.bg_image} src="/seafood-paella.jpg" />
           <div className={styles.bg_overlay} />
+          <div className={styles.content}>
+            <p className={styles.description}>
+              Creamy cheddar cheese mixed with perfectly cooked macaroni, topped
+              with crispy breadcrumbs. A classic comfort food.
+            </p>
+            <p className={styles.price}>$12.99</p>
+          </div>
         </div>
       </article>
     </div>
