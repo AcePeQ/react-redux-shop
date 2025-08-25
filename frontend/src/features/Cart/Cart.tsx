d19d4c8fdb34c16d68ac7a1e27705f363e1de46d
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-25 14:54:40 +0200

    feat(ui): cart summary

--- frontend/src/features/Cart/Cart.tsx
+++ frontend/src/features/Cart/Cart.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector } from "../../hooks/storeHooks";
 import styles from "./Cart.module.css";
 import CartItem from "./CartItem/CartItem";
+import CartSummary from "./CartSummary/CartSummary";
 
 function Cart() {
   const cart = useAppSelector((state) => state.cart.cart);
@@ -12,6 +13,7 @@ function Cart() {
           <CartItem key={item.id} item={item} />
         ))}
       </ul>
+      <CartSummary />
     </div>
   );
 }
---
+++ frontend/src/features/Cart/CartSummary/CartSummary.tsx
@@ -0,0 +1,40 @@
+import Button from "../../../components/Button/Button";
+import { useAppSelector } from "../../../hooks/storeHooks";
+import { currencyFormater } from "../../../utils/utilsFunctions";
+import styles from "./CartSummary.module.css";
+
+function CartSummary() {
+  const cart = useAppSelector((state) => state.cart.cart);
+
+  const totalPrice = cart.reduce(
+    (acc, val) => (acc += Number(val.price) * val.quantity),
+    0
+  );
+
+  const deliverPrice = 5;
+
+  return (
+    <div className={styles.summary}>
+      <div className={styles.summarySticky}>
+        <div className={styles.summaryRow}>
+          <p>Subtotal</p>
+          <p>{currencyFormater(totalPrice)}</p>
+        </div>
+
+        <div className={styles.summaryRow}>
+          <p>Deliver</p>
+          <p>{currencyFormater(deliverPrice)}</p>
+        </div>
+
+        <div className={styles.summaryRow}>
+          <p>Total</p>
+          <p>{currencyFormater(totalPrice + deliverPrice)}</p>
+        </div>
+
+        <Button>Checkout</Button>
+      </div>
+    </div>
+  );
+}
+
+export default CartSummary;
