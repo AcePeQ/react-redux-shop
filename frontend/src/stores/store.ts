7552eeb8f1b1bcd4b5f07514da221d8ef16d5668
AcePeQ <maciej.nojszewski22@gmail.com>
2025-08-25 18:22:29 +0200

    feat(store): add  progress reducer to the global store

--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "../features/Menu/menuSlice";
 import cartReducer from "../features/Cart/cartSlice";
+import progressReducer from "../features/Cart/cartProgressSlice";
 
 const store = configureStore({
   reducer: {
     menu: menuReducer,
     cart: cartReducer,
+    progress: progressReducer,
   },
 });
 
